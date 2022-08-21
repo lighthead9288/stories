08e8ea9d4441e946a34e55564b943b463922f751
chromatec-soft <chromatecSoft@gmail.com>
2022-08-21 23:56:35 +0300

    First commit

---
+++ lib/models/story_data.dart
@@ -0,0 +1,15 @@
+class StoryData {
+  final int backgroundColor;
+ // final String imgUrl;
+  final String title;
+  final String subtitle;
+  final bool containsButtons;
+
+  StoryData({
+    required this.backgroundColor,
+   // required this.imgUrl,
+    required this.title,
+    required this.subtitle,
+    required this.containsButtons
+  });
+}
